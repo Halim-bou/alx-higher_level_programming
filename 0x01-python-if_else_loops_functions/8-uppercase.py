04425150f4231be8d3225c0fc5cee983f1aba2a9
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-08-08 16:34:49 -0700

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for s in str:
+        if 'a' >= s >= 'z':
+            s = chr(ord(s) - ord('a') + ord('A'))
+        print("{}".format(s), end="")
+        print("")

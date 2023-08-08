72564a89e760f8475aa28719f0f42eff9e38e5ad
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-08-08 16:42:30 -0700

    change permission

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for s in str:
-        if 'a' >= s >= 'z':
-            s = chr(ord(s) - ord('a') + ord('A'))
+        if 'a' >= s <= 'z':
+            s = chr(ord(s) - 32)
         print("{}".format(str), end="")
     print("")

b4601dfe9e5df1fd9524494e385432187c1e7401
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-08-09 10:37:49 -0700

    algorithm for check if there is any cycle in a linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * check_cycle - function that check if there is any cycle in the linked list
+ * @list: The head pointer to the linked list
+ * Return: 0 if there is no cycle or 1 if a cycle found
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slower_ptr;
+	listint_t *faster_ptr;
+
+	if (!list || !list->next)
+		return (0);
+	*slower_ptr = list;
+	*faster_ptr = list->next;
+	while (faster_ptr && faster_ptr->next)
+	{
+		if (slower_ptr == faster_ptr)
+			return (1);
+		slower_ptr = slower_ptr->next;
+		faster_ptr = faster_ptr->next->next;
+	}
+	return (0);
+}

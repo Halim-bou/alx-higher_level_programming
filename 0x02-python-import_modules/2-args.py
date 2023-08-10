ae871424fb89729e5aed1095c441a9908915b8e5
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-08-10 14:00:01 -0700

    add variable lenght

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    lenght = len(sys.argv) - 1
-    if lenght == 0:
-        print("0 arguments.")
-    elif lenght == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(lenght))
-    for i in range(lenght):
-        print("{} : {}".format(i + 1, sys.argv[i + 1]))
+import sys
+lenght = len(sys.argv) - 1
+if lenght == 0:
+     print("0 arguments.")
+elif lenght == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(lenght))
+for i in range(lenght):
+    print("{} : {}".format(i + 1, sys.argv[i + 1]))

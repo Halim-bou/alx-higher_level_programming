447df3def82e34fb1803d27ead328444695ef079
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-08-13 12:35:12 -0700

    fix the path

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
-#!/usr/bin/pytho3
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list:
-        copy_list = my_list[:]
-        if idx < 0 or idx > len(my_list) - 1:
-            return copy_list
-        else:
-            copy_list[idx] = element
-            return copy_list
+    copy_list = my_list[:]
+    if idx < 0 or idx > len(my_list) - 1:
+        return copy_list
+    else:
+        copy_list[idx] = element
+        return copy_list

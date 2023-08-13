b15b9f99c7132cab7e4aa2b1278aea54f1edc4eb
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-08-13 12:26:18 -0700

    i don't now what is the problem

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/pytho3
 def new_in_list(my_list, idx, element):
     if my_list:
-        copy_list = my_list.copy()
+        copy_list = my_list[:]
         if idx < 0 or idx > len(my_list) - 1:
-            return my_list.copy()
+            return copy_list
         else:
             copy_list[idx] = element
             return copy_list

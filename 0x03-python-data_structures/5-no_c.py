8e28746e06ba029d5c53686082709b28abe60387
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-08-13 12:47:43 -0700

    fix some errors

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = my_string.translate({ord('c' and 'C') : NONE})
+    new_str = my_string.translate({ord('c' and 'C'): None})
     return new_str

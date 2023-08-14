67f1cc5572d3792fdf971b30511b895d6749dddf
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-08-14 11:43:15 -0700

    fix pycodestyle

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     the_tuple = ()
     if len(sentence) == 0:
-        the_tuple = 0 ,"None"
+        the_tuple = 0, "None"
         return the_tuple
     else:
         the_tuple = len(sentence), sentence[0]

369a464c82575b7f7f579e93d2c53b29a54517e8
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-08-14 11:41:10 -0700

    fuction that count the legth of string and return it with the first char

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    the_tuple = ()
+    if len(sentence) == 0:
+        the_tuple = 0 ,"None"
+        return the_tuple
+    else:
+        the_tuple = len(sentence), sentence[0]
+        return the_tuple

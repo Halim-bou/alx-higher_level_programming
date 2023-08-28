4d1b420a70b9a8d0415a48e3ecdd317d280c9f8d
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-08-28 05:55:20 -0700

    fonction that print just integer value

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        if int(value):
+            print("{:d}".format(value))
+            return True
+    except Exception as err:
+        pass
+    return False

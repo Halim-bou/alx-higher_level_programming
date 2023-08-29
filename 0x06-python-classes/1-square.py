d4f8408106af80c2551e62ddeb82884a0a947c8a
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-08-29 15:16:20 -0700

    define a class and privet instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""square classe defining"""
+
+
+class Square:
+    """intialize a new square"""
+
+    def __init__(self, size):
+        """private instance attribute size"""
+        self.__size = size

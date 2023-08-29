d9b80d19caea9ece620b07e4958abecc62e111a0
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-08-29 15:50:12 -0700

    define class with exception

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define a class square"""
+
+
+class Square:
+    """initialize a new square"""
+    def __init__(self, size=0):
+        """private instance attribute size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size

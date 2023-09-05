be4768230c57afde5e8a0362126cb1159fd4f953
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-04 21:18:39 -0700

    fixing all

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("width maust be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 

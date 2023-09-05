338fce48c3b389793287b4dba032a8bb491f466d
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-05 16:03:59 -0700

    trying with getattr

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
-#1/usr/bin/python3
+#!/usr/bin/python3
 def magic_string():
     magic_string.lenth = getattr(magic_string, 'lenth', 0) + 1
     return (', '.join(["BestSchool" for i in range(magic_string.lenth)]))

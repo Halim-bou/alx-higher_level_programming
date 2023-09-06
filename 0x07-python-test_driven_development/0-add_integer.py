1eac19f8ea3109c07e62f8dc8747ecb113577016
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-06 09:16:00 -0700

    trying to remove documentation module

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,2 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    """add functione:
-    That return the result in type ineteger if type of
-    argumentareinteger or float else it will raise a typeerror
-    """
-    if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be an integer")
-    elif not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be an integer")
-    else:
-        return (int(a) + int(b))

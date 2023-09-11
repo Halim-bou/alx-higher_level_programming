bd8bfeabebbc2a8d95e09c8b8a96867da86812cf
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-11 13:25:33 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+creat a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """function for the class"""
+    def area(self):
+        """raise exceptions"""
+        raise Exception("area() is not implemented")

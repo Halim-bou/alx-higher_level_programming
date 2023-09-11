e8caf1ace89cf412d68f2ad447f5f0f8ea1c0431
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-11 13:14:11 -0700

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!.usr/bin/python3
+"""
+functio inherits_from
+"""
+
+
+def inherits_from(obj, a_class):
+    """check if the object is an instance of a class that inherited"""
+    if issubclass(obj.__class__, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False

bfd23f6abedea5d50cdb250abf52676ef21830b5
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-12 15:15:45 -0700

    make function return number of bytes

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ func toopen and write in a text file
 def write_file(filename="", text=""):
     """write in a file if it doesn't exist creat a file"""
     with open(filename, 'w', encoding="utf-8") as f:
-        text_to_write = f.write(text)
+        return f.write(text)

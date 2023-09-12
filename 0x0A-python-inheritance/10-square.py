c9d0a9e344305aa7b52bffdf1b8b2167ad9c710a
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-11 18:30:00 -0700

    trying to find the bug

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
         """define arguments and check inteer validator"""
         self.integer_validator("size", size)
         self.__size = size
-        supe().__init__(size, size)
+        super().__init__(size, size)
 
     def area(self):
         """square operation"""

9b72fb3d733769dbd6f5512b366a0f95570a3f08
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-11 18:23:48 -0700

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a multiple inheritance class
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """function implemente"""
+    def __init__(self, size):
+        """define arguments and check inteer validator"""
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """square operation"""
+        return self.__size ** 2

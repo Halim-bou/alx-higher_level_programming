3e87584f7486751a89744d5aaa9021f8b1882533
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-12 16:39:51 -0700

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+playing with json and io file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """creat an object from json file"""
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)

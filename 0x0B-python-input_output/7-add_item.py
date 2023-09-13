5e06bdac68f40dd80fa370eae7a6a7d88f7d1d0f
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-12 18:18:56 -0700

    i don't know the bug where is

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+io and json 
+"""
 import sys
 import json
 import os.path
@@ -6,7 +9,7 @@ import os.path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+filename = 'add_item.json'
 if os.path.isfile(filename):
     filelist = load_from_json_file(filename)
 else:

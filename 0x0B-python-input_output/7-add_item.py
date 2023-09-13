c024e5fdc92ba9b6c3444237cdc94393cf98cacd
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-12 18:23:09 -0700

    some pycodestyle fixing

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
-io and json 
+io and json
 """
 import sys
 import json
 import os.path
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 

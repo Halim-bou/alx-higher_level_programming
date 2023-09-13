d9e21d32e2041cd1bb21d890332d5f7dc257d7b3
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-09-12 19:22:34 -0700

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+creat a Student class
+"""
+
+
+class Student:
+    """function for the class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation"""
+        if attrs is None:
+            return self.__dict__
+        attrs_check = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                attrs_check[attr] = getattr(self, attr)
+        return attrs_check

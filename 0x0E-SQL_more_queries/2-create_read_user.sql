bf915d32a9d24cb75612f3b66cad360e3f032dd0
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-10-18 18:28:51 -0700

    task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- create database and a user
+-- the user have only select privilege in the database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';

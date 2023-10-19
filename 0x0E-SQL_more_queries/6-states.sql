75e4f76e08b952be357bc3beb69059e1aa51d2ff
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-10-18 18:49:52 -0700

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- script that create database and table on it
+-- is is int and unique and can't be null and autogenarated and he is aprimary key, name can't be null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `states`.`hbtn_0d_usa`(
+	PRIMARY KEY(`id`),
+	id INT NOT NULL AUTO_INCREMENT UNIQUE,
+	name VARCHAR(256) NOT NULL);

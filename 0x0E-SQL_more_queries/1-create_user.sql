72b2711d086c2a7f35b70a955f9c489ae768f74b
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-10-18 18:13:49 -0700

    fixing errors in task1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- script that creat the MySQL server user 'user_0d_1' with all privileges
 -- set for it a password
-CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT ON *.* TO 'user_0d_1';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

0913a184051db43818ea9058e246c46fe8ac2081
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-10-18 18:10:48 -0700

    task 0 and 1

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- scripte that lists all privileges of the MySQL users "user_0d_1, user_0d_2
+-- using show grant command
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

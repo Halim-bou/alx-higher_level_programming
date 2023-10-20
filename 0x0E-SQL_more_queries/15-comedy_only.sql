b269d71d1ec713157080cf76758516507a90b08b
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-10-19 19:18:05 -0700

    fixing errors in task 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- script to lists all Comedy shows in the database hbtn_0d_tvshows
 -- Each record display tv_shows.title and sorted in ascending order by the show title
 SELECT tv.title
-FROM tv_shows AS t
+FROM tv_shows AS tv
 INNER JOIN tv_show_genres AS s
 ON tv.id = s.show_id
 INNER JOIN tv_genres AS g

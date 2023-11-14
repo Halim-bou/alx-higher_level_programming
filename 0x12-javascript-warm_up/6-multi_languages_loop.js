2fb777b87d2dd0c48d58c965f8b66dd77606e6b0
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-11-14 11:51:55 -0800

    task 6 and 7

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const ars = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const ar in ars) {
+  console.log(ars[ar]);
+}
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const ar = process.argv[2];
+
+if (isNaN(ar)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < ar; i++) {
+    console.log('Cis fun');
+  }
+}

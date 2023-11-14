4fcc1b8f6117c23b195d28800e79860d4cbffea1
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-11-14 12:30:40 -0800

    task 11 and 12

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arg = process.argv.silce(2);
+let secondBigg = 0;
+
+if (arg.length > 1) {
+  const integers = arg.map(Number);
+  integers.sort((a, b) => b - a);
+  secondBigg = integers[1];
+}
+console.log(secondBigg);

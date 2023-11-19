7361723c4a93be6eb56dc069e2d03e0cfe61552d
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-11-19 13:10:29 -0800

    looking for the bug

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,6 +12,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """representation for State"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)

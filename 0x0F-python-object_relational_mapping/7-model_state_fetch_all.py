c8196f187eba903c4d4d86f92dd3bc08d6f76230
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-11-19 17:47:33 -0800

    try again

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,10 +2,8 @@
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State, Base
-"""
-script that lists all State objects from the database hbtn_0e_6_usa
-"""
+from model_state import Base, State
+
 
 
 if __name__ == "__main__":

bb4b835d4dd80aa7104599c511dabc20eab0f726
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-11-19 18:13:43 -0800

    ???

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+lists all state
+"""
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,12 +10,9 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    myeng = create_engine('mysql+mysqldb://{}:{}\
-            @localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    #db = create_engine(myeng, pool_pre_ping=True)
-    #Base.metadata.create_all(db)
+    myeng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(myeng)
     Session = sessionmaker(bind=myeng)
     session = Session()
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
-    #session.close()
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+"""
+lists the first state
+"""
 from model_state import State, Base
 from sys import argv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmake
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}\
-            ".format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
 

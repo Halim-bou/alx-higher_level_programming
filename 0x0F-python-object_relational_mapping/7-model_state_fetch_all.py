a473fabfb3903f4c4cf55bf297f053d4b2180011
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-11-19 18:05:29 -0800

    where is the bug??

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,7 +9,7 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     myeng = create_engine('mysql+mysqldb://{}:{}\
-            @localhost/{}'.format(argv[1], argv[2], argv[3]), pool_ping=True)
+            @localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     #db = create_engine(myeng, pool_pre_ping=True)
     #Base.metadata.create_all(db)
     Session = sessionmaker(bind=myeng)

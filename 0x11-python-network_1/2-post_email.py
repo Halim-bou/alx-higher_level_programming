67f32be049539f24eb45bdc32039eca64f5ef4b1
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-12-01 15:29:45 -0800

    looking for bugs

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,9 +8,10 @@ import urllib.request
 import sys
 
 
-url = sys.argv[1]
-value = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(value).encode("ascii")
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as resp:
-    print(resp.read().decode("utf-8"))
+if name == "__main__":
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as resp:
+        print(resp.read().decode("utf-8"))

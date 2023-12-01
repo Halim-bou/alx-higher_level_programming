9496baf740fc7a43a6cc7828a7ec6613fa77533d
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-12-01 15:26:26 -0800

    looking for bugs

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,11 +3,11 @@
 task 1
 """
 
-from sys import argv
+import sys
 import urllib.request
 
 
-url = argv[1]
+url = sys.argv[1]
 req = urllib.request.Request(url)
 with urllib.request.urlopen(req) as resp:
     print(dict(resp.headers).get("X-Request-Id"))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,11 +5,11 @@ task 2
 
 import urllib.parse
 import urllib.request
-from sys import argv
+import sys
 
 
-url = argv[1]
-value = {'email': argv[2]}
+url = sys.argv[1]
+value = {'email': sys.argv[2]}
 data = urllib.parse.urlencode(value).encode("ascii")
 req = urllib.request.Request(url, data)
 with urllib.request.urlopen(req) as resp:

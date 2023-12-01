58a0756afae7b3dd05ea1f1a32f790a087aa94aa
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-12-01 15:28:08 -0800

    looking for bugs

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,8 +6,8 @@ task 1
 import sys
 import urllib.request
 
-
-url = sys.argv[1]
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as resp:
-    print(dict(resp.headers).get("X-Request-Id"))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as resp:
+        print(dict(resp.headers).get("X-Request-Id"))

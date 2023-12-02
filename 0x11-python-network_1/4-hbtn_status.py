1a29b523a5cc86a88e95c7b8916e500a1fe9f168
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-12-01 17:59:53 -0800

    fixing errors for task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 
 
 if __name__ == "__main__":
-    req = requests.get("https://intranet.hbtn.io/status")
+    req = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print(f"\t- type: {type(req.text)}")
     print(f"\t- content: {req.text}")

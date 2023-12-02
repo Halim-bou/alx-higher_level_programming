a2f1248722a6d6aeff6f912041a43ac8bf78375c
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-12-01 17:53:23 -0800

    fixing errors for task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,7 +7,7 @@ import urllib.request
 import urllib.error
 
 
-if _name__ == "__main__":
+if __name__ == "__main__":
     try:
         with urllib.request.urlopen(sys.argv[1]) as req:
             print(req.read().decode("utf-8"))

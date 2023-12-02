0e69739b17ab820e52d957b65267c846b5b6196c
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-12-01 18:32:50 -0800

    fixing bugs

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,7 @@ from sys import argv
 from requests.auth import HTTPBasicAuth
 
 
-if __name__ == "__main_-":
-    author = HTTPBasicAuth(argv[1], argv[2])
-    req = requests.get("https://api.github.com/user", auth=author)
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
     print(req.json().get("id"))

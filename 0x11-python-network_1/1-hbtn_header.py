6700a1022baac8641421cdd2d25dd29d6a5226a6
aelbouaa <hbouaami@gmail.com>
2024-03-01 19:23:17 +0100

    func that fetsh specific object from headers

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+'''fetsh header object'''
 import urllib.request
 import sys
 
 if __name__ == '__main__':
-    url = sys.aargv[1]
+    url = sys.argv[1]
     with urllib.request.urlopen(url) as r:
         object = r.headers.get('X-Request-Id')
         print(object)

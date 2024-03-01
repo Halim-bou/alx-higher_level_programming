5f295175717848b81a265af01b0da7ec06061f4e
aelbouaa <hbouaami@gmail.com>
2024-03-01 19:08:01 +0100

    first task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,8 +8,8 @@ try:
         content = r.read()
         utf8_content = content.decode('utf-8')
         print("Body response:")
-        print("\t-type: ", type(content))
-        print("\t-content: ", content)
-        print("\t-utf8 content: ", utf8_content)
+        print("\t-type:", type(content))
+        print("\t-content:", content)
+        print("\t-utf8 content:", utf8_content)
 except urllib.error.URLError as e:
-    print("Error: ", e)
+    pass

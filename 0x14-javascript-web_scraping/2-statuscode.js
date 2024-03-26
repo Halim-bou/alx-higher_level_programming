3d24a9d5aa409c7c617802fece1c94ee40cddb5a
aelbouaa <hbouaami@gmail.com>
2024-03-26 21:07:47 +0000

    check with on()

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 const url = process.argv[2];
 // funcion to request a GET
 request.get(`${url}`).on('response', function (response) {
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
 });

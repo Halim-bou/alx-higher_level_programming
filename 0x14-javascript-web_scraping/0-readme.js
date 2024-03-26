7207162efcf19c1ec2fa9a22ccffc25854c49946
aelbouaa <hbouaami@gmail.com>
2024-03-26 20:14:24 +0000

    check again

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,20 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const file = process.argv[2];
+// funcion to read the file
+function readF (path) {
+  fs.readFile(`./${path}`, 'utf-8', (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+    }
+  });
+}
 
-fs.readFile(`./${process.argv[2]}`, 'utf-8', (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
-  }
-});
+if (!file) {
+  process.exit(1); // Exit with an error code (1)
+} else {
+  readF(file);
+}

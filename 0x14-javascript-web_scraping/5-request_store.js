bb7fb3e7e1560315c2f5453ff3c546c91586070e
aelbouaa <hbouaami@gmail.com>
2024-03-27 02:07:50 +0000

    print data in file from url

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+
+request(url, function (error, response, data) {
+    if (error) {
+        console.error(error);
+    }
+    fs.writeFile(`${filePath}`, `${data}`, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    });
+});
+
